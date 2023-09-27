030303eb6147de9568553c02d52d54f3e845c64e
mbari60 <kevinmbari600@gmail.com>
2023-09-27 20:22:29 +0300

    functions lab assignment corected

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
-// Code your solution in this file!
+const hQ = 42;
 function distanceFromHqInBlocks(pickUpPoint) {
-  const hQ = 42;
-  distanceFromHqInBlocks = Math.abs(pickUpPoint - hQ);
-
+  const distanceFromHqInBlocks = Math.abs(pickUpPoint - hQ);
   return distanceFromHqInBlocks;
 }
 
 function distanceFromHqInFeet(pickUpPoint) {
-  const hQ = 42;
-  distanceFromHqInFeet = Math.abs(pickUpPoint - hQ) * 264;
+  const distanceInBlocks = distanceFromHqInBlocks(pickUpPoint);
+  const distanceFromHqInFeet = distanceInBlocks * 264;
   return distanceFromHqInFeet;
 }
 
- function distanceTravelledInFeet(start, destination) {
-   distanceTravelledInFeet = Math.abs(start - destination) * 264;
-
-   return distanceTravelledInFeet;
- }
-
+function distanceTravelledInFeet(start, destination) {
+  const distance = Math.abs(start - destination) * 264;
+  return distance;
+}
 
 function calculatesFarePrice(start, destination) {
-  calculatesFarePrice = Math.abs(start - destination);
-  distanceTravelledInFeet = calculatesFarePrice * 264;
-  let freeSample = 400;
-  if (distanceTravelledInFeet < 400) {
-    return 0;
-  } else if (
-    distanceTravelledInFeet >= 400 &&
-    distanceTravelledInFeet <= 2000
-  ) {
-    return (calculatesFarePrice = 0.02 * (distanceTravelledInFeet - freeSample));
-  } else if (
-    distanceTravelledInFeet > 2000 &&
-    distanceTravelledInFeet <= 2500
-  ) {
-    return 25;
-  } else if (distanceTravelledInFeet > 2500) {
-    return "cannot travel that far";
+  const freeSample = 400;
+  const distance = distanceTravelledInFeet(start, destination);
+  let fare;
+
+  if (distance <= freeSample) {
+    fare = 0;
+  } else if (distance <= 2000) {
+    fare = 0.02 * (distance - freeSample);
+  } else if (distance <= 2500) {
+    fare = 25;
+  } else {
+    fare = "cannot travel that far";
   }
+
+  return fare;
 }
